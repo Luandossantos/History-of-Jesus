b70fe2d7ee648701d675ddce5c54ba8e23161462
cleitoncordeirolima <103213932+cleitoncordeirolima@users.noreply.github.com>
2024-07-31 10:23:56 -0300

    Atualizando pag.

--- scrip.js
+++ scrip.js
@@ -8,54 +8,106 @@ const perguntas = [
     {
         enunciado: "QUANTOS MAGOS FORAM VISITAR JESUS EM SEU NASCIMENTO ?",
         alternativas: [
-            "5.",
-            "3."
+            {
+                texto: "5",
+                afirmacao: "FALSA"
+            },
+            {
+                texto: "3",
+                afirmacao: "VERDADEIRA"
+            }
         ]
     },
     {
         enunciado: "QUAL ERA A PROFISSÃO DO PAI DE JESUS ?",
         alternativas: [
-            "Carpinteiro.",
-            "Pescador."
+            {
+                texto: "Carpinteiro",
+                afirmacao: "VERDADEIRA"
+            },
+            {
+                texto: "Pescador",
+                afirmacao: "FALSA"
+            }
         ]
     },
     {
         enunciado: "QUAL FOI O LIVRO QUE JESUS ESCREVEU ?",
         alternativas: [
-            "BIblía.",
-            "Alcorão."
+            {
+                texto: "Biblía",
+                afirmacao: "FALSA"
+            },
+            {
+                texto: "Alcorão",
+                afirmacao: "VERDADEIRA"
+            }
         ]
     },
     {
-        enunciado: "QUANTOS DISCÍPULOS JESUS TINHA ?",
+        enunciado: "QUANTOS DICÍPULOS JESUS TINHA ?",
         alternativas: [
-            "12.",
-            "11."
+            {
+                texto: "12",
+                afirmacao: "VERDADEIRA"
+            },
+            {
+                texto: "11",
+                afirmacao: "FALSA"
+            }
         ]
     },
     {
         enunciado: "QUAL A CIDADE QUE JESUS NASCEU ?",
         alternativas: [
-            "Jerusalém.",
-            "Belém."
+            {
+                texto: "Jerusalém",
+                afirmacao: "FALSA"
+            },
+            {
+                texto: "Belém",
+                afirmacao: "VERDADEIRA"
+            }
         ]
     },
 ];
 
+
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
-function mostraAlternativas() {
+
+function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
         const botaoAlternativas = document.createElement("button");
-        botaoAlternativas.textContent = alternativa;
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild(botaoAlternativas);
     }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "SEUS RESULTADOS:";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
 }
 
 mostraPergunta();
