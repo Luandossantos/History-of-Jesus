8fb866f30be3dd366d719bc0166f9f834d38c00c
cleitoncordeirolima <103213932+cleitoncordeirolima@users.noreply.github.com>
2024-09-03 07:33:26 -0300

    Add files via upload

--- scrip.js
+++ scrip.js
@@ -10,11 +10,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "5",
-                afirmacao: "FALSA, NÃO FOI DESTA VEZ ESTAVAM PRESENTES EM SEU NASCIMENTO 3 REIS MAGOS;"
+                afirmacao:[
+                     "FALSA, NÃO FOI DESTA VEZ ESTAVAM PRESENTES EM SEU NASCIMENTO 3 REIS MAGOS;",
+                     "FALSA, NO NASCIMENTO DE JESUS ESTAVAM PRESENTES 3 REIS MAGOS E NÃO 5;"
+                    ]
             },
             {
                 texto: "3",
-                afirmacao: "VERDADEIRO,ISTO MESMO ESTAVAM PRESENTES NO NASCIMENTO DO MENINO JESUS 3 REIS MAGOS;"
+                afirmacao:[ 
+                    "VERDADEIRO,ISTO MESMO ESTAVAM PRESENTES NO NASCIMENTO DO MENINO JESUS 3 REIS MAGOS;",
+                    "VERDADEIRO, CORRETO NO NASCIMENTOS DE JESUS ESTAVAM PRESENTES 3 REIS MAGOS;"
+                ]
             }
         ]
     },
@@ -23,24 +29,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Carpinteiro",
-                afirmacao: "Verdadeira, a profissão do pai de Jesus era carpinteiro;"
+                afirmacao: [
+                    "Verdadeira, a profissão do pai de Jesus era carpinteiro;",
+                    "Verdadeira, sim, o pai de Jesus era carpinteiro;"
+                ]
             },
             {
                 texto: "Pescador",
-                afirmacao: "Falsa, a profissão do pai de Jesus era carpinteiro e não pescador;"
-            }
-        ]
-    },
-    {
-        enunciado: "QUAL FOI O LIVRO QUE JESUS ESCREVEU ?",
-        alternativas: [
-            {
-                texto: "Biblía",
-                afirmacao: "FALSA, O LIVRO QUE JESUS ESCREVEU NÃO FOI A BIBLÍA E SIM ALCORÃO;"
-            },
-            {
-                texto: "Alcorão",
-                afirmacao: "VERDADEIRA, ISTO MESMO! O LIVRO QUE JESUS CRISTO ESCREVEU FOI O ALCORÃO;"
+                afirmacao: [
+                    "Falsa, a profissão do pai de Jesus era carpinteiro e não pescador;",
+                    "Falsa, o pai de Jesus era um carpinteiro;"
+                ]
             }
         ]
     },
@@ -49,11 +48,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "12",
-                afirmacao: "Verdadeira, exato! Jesus tinha 12 discípulpos; "
+                afirmacao: [
+                    "Verdadeira, exato! Jesus tinha 12 discípulos;",
+                    "Verdadeira, isto mesmo! tinha 12 discípulos;"
+                ]
             },
             {
                 texto: "11",
-                afirmacao: "Falsa, não foi desta vez... Jesus tinha 12 discípulos;"
+                afirmacao: [
+                    "Falsa, não foi desta vez... Jesus tinha 12 discípulos;",
+                    "Falsa, Jesus não tinha 12 discípulos e não 11;"
+                ]
             }
         ]
     },
@@ -62,11 +67,17 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Jerusalém",
-                afirmacao: "FALSA, JESUS NASCEU NA CIDADE DE BELÉM."
+                afirmacao: [
+                    "FALSA, JESUS NASCEU NA CIDADE DE BELÉM.",
+                    "FALSA, JESUS NÃO NASCEU NA CIDADE DE JERUSALÉM;"
+                ]
             },
             {
                 texto: "Belém",
-                afirmacao: "VERDADEIRA, JESUS NASCEU NA CIDADE DE BELÉM."
+                afirmacao: [
+                    "VERDADEIRA, JESUS NASCEU NA CIDADE DE BELÉM.",
+                    "VERDADEIRA, CORRETO! JESUS NASCEU NA CIDADE DE BELÉM;"
+                ]
             }
         ]
     },
@@ -98,7 +109,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada) {
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
@@ -108,6 +119,11 @@ function mostraResultado() {
     caixaPerguntas.textContent = "SEUS RESULTADOS:";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+}
+
+function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista [posicao];
 }
 
 mostraPergunta();
